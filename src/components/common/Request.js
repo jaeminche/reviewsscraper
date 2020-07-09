c8da80e26eba6c7257501051264763b53ea38e04
Jae M. Choi <jaeminche@gmail.com>
2020-07-10 01:43:37 +0900

    feat: 잡플래닛 평점 스크레이핑 초기 모듈 완료

--- src/components/common/Request.js
+++ src/components/common/Request.js
@@ -1,31 +1,47 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import Request from "react-http-request";
 import ReactHtmlParser /*, { processNodes, convertNodeToElement, htmlparser2}*/ from "react-html-parser";
 
-export default class App extends Component {
-  render() {
-    const companyname = "비바리퍼블리카";
-    return (
-      <Request
-        url={`https://www.jobplanet.co.kr/search?query=${companyname}`}
-        method="get"
-        // accept="application/json"
-        verbose={true}
-      >
-        {({ error, result, loading }) => {
-          if (loading) {
-            return <div>loading...</div>;
-          } else {
-            console.log(result.text, ReactHtmlParser(result.text));
-            return (
-              <div>
-                {/* {JSON.stringify(result)} */}
-                {ReactHtmlParser(result.text)}
-              </div>
-            );
-          }
-        }}
-      </Request>
-    );
-  }
-}
+const App = () => {
+  const [searchword, setSearchword] = useState("");
+  const onChange = e => {
+    e.preventDefault();
+    console.log("==13", e.target.value);
+    setSearchword(e.target.value);
+  };
+  return (
+    <Request
+      url={`https://www.jobplanet.co.kr/search?query=${searchword}`}
+      method="get"
+      // accept="application/json"
+      verbose={true}
+    >
+      {({ error, result, loading }) => {
+        if (loading) {
+          return <div>loading...</div>;
+        } else if (result !== null) {
+          console.log("==12", result);
+
+          const retrieved = result.text;
+          const startIndex = retrieved.indexOf('rate_ty02">');
+          const sliced = retrieved.slice(startIndex + 11, startIndex + 20);
+          const endIndex = sliced.indexOf("</span");
+          console.log("==11", sliced, endIndex);
+          return (
+            <div>
+              <input value={searchword} onChange={e => onChange(e)}></input>
+              {searchword}의 평점은 {sliced.slice(0, endIndex)}입니다
+            </div>
+          );
+        } else {
+          return (
+            <div>
+              <input value={searchword} onChange={e => onChange(e)}></input>
+            </div>
+          );
+        }
+      }}
+    </Request>
+  );
+};
+export default App;
